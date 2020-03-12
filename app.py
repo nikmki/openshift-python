24e2a86b5d511cbb50b720adf26606ba23dad5be
nikmki <nimaki007@gmail.com>
2020-03-12 15:24:14 +0100

    hey

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-import time
+import sys, os, socket, time
+import urllib.request
 
-for i in range(10):
-    print("Im line number", i)
-    time.sleep(2)
+print(urllib.request.urlopen('http://ip.42.pl/raw').read())
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_address = ("", 5000)
+
+
+sock.bind(server_address)
+sock.listen(1)
+
+print("waiting...")
+
+connection, client_address = sock.accept()
+print("Connected to :", client_address)
+
+
+while True:
+    data = connection.recv(1024)
+    if data:
+        msg = data.decode()
+        print(msg)
